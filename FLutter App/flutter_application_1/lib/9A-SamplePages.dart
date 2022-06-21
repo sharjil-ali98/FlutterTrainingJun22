f7557cb2e5e44f3dd1457b98a86b600f4334af3e
pankkap <pankkap@gmail.com>
2022-06-21 10:32:02 +0530

    Day-18 Code

---
+++ FLutter App/flutter_application_1/lib/9A-SamplePages.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Home Page",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "About Page",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class ServicePage extends StatelessWidget {
+  const ServicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Service Page",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
--- FLutter App/flutter_application_1/lib/main.dart
+++ FLutter App/flutter_application_1/lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/10-ListTileDemo.dart';
+import 'package:flutter_application_1/9-BottomNavigationBarDemo.dart';
 import 'package:flutter_application_1/Screens/HomeScreen.dart';
 import 'package:flutter_application_1/Screens/Screen1.dart';
 import 'package:flutter_application_1/Screens/Screen2.dart';
@@ -10,14 +12,16 @@ import 'Screens/Screen0.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: HomeScreen(),
-      routes: {
-        'S0': (context) => Screen0(title: "Settings"),
-        'S1': (context) => Screen1(
-              title: 'Gallery',
-            ),
-        'S2': (context) => Screen2(title: 'About Phone'),
-      },
+      // home: HomeScreen(),
+      home: BottomNavigationDemo(),
+      // home: ListTileDemo(),
+      // routes: {
+      //   'S0': (context) => Screen0(title: "Settings"),
+      //   'S1': (context) => Screen1(
+      //         title: 'Gallery',
+      //       ),
+      //   'S2': (context) => Screen2(title: 'About Phone'),
+      // },
     ),
   );
 }
